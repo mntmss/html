677cbf219625e3e150d0e850039bd89bdff63b49
mntmss <34361593+mntmss@users.noreply.github.com>
2023-11-28 11:45:20 +0800

    Create tools.php

---
+++ tools.php
@@ -0,0 +1,16 @@
+<?php
+$data = [
+    "ec"   => 0,
+    "em"   => "success",
+    "data" => [
+        "showTools" => true,
+        "toolsList" => [
+            [
+                "pic"         => "https://lh3.googleusercontent.com/S05xzaU9eDDKLsx-eBloWDW93pJOXPvoJfYUKMNLFvhaNVEQqXe6FKf5JjjnUIa0DsI",
+                "url"         => "https://play.google.com/store/apps/details?id=cn.wxhyi.usagetime.cleanmaster",
+                "packageName" => "cn.wxhyi.usagetime.cleanmaster"
+            ]
+        ],
+    ],
+];
+echo json_encode($data);
