4a0301ac8ceda823297cfd6eec2f5c0014776e09
mntmss <34361593+mntmss@users.noreply.github.com>
2023-11-29 15:24:56 +0800

    Update tools.php

--- tools.php
+++ tools.php
@@ -9,6 +9,11 @@ $data = [
                 "pic"         => "https://lh3.googleusercontent.com/S05xzaU9eDDKLsx-eBloWDW93pJOXPvoJfYUKMNLFvhaNVEQqXe6FKf5JjjnUIa0DsI",
                 "url"         => "https://play.google.com/store/apps/details?id=cn.wxhyi.usagetime.cleanmaster",
                 "packageName" => "cn.wxhyi.usagetime.cleanmaster"
+            ],
+            [
+                "pic"         => "https://lh3.googleusercontent.com/of5mpZlt22EQiT5TJMWSuLsYPR7JyFsb2xaEmAG8i-DlQl8OXLEYhH4Foo93AqonYxg",
+                "url"         => "https://play.google.com/store/apps/details?id=cn.wxhyi.usagetime.tkdownloader",
+                "packageName" => "cn.wxhyi.usagetime.tkdownloader"
             ]
         ],
     ],
