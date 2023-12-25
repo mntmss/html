14ee6e1bff20850f306c6e144a6d4d67c4d0e4f0
mntmss <34361593+mntmss@users.noreply.github.com>
2023-12-25 12:11:37 +0800

    Update info.php

--- info.php
+++ info.php
@@ -3,7 +3,9 @@ $data = [
     "ec"   => 0,
     "em"   => "success",
     "data" => [
-        "lastVersion" => 10
+        "lastVersion" => 10,
+        "url":"",
+        "desc":""
     ]
 ];
 echo json_encode($data);
