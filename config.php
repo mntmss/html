de2b4b368df1078acd6368bc2ecc4d4273ff69cc
mntmss <34361593+mntmss@users.noreply.github.com>
2023-12-25 12:24:08 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,1 +1,11 @@
-
+<?php
+$data = [
+    "ec"   => 0,
+    "em"   => "success",
+    "data" => [
+        "lastVersion" => 10,
+        "url":"",
+        "desc":""
+    ]
+];
+echo json_encode($data);
